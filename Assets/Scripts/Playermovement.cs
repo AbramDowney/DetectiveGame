6c8b9a0da4ede1609f400ebdc9b36eb478ca458e
AbramDowney <134163475+AbramDowney@users.noreply.github.com>
2024-05-18 11:05:08 -0400

    Push

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public Rigidbody playerRB;
+
+    public float forwardSpeed;
+    public float LRSpeed;
+
+    void Start() {
+         
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w")){
+            playerRB.AddForce(0, 0, forwardSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey("s")){
+            playerRB.AddForce(0, 0, -forwardSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey("d")){
+            playerRB.AddForce(LRSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("a")){
+            playerRB.AddForce(-LRSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
